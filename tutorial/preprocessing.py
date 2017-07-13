71b096306842c64fb26bae24b0675d1b79f5d0ba
Cuong Nguyen <cuong.vn08@gmail.com>
2017-07-13 23:44:54 +0900

    upload the 1st tutorial.

---
+++ tutorial/preprocessing.py
@@ -0,0 +1,70 @@
+'''
+Reference: https://www.kaggle.com/gzuidhof/full-preprocessing-tutorial
+'''
+
+import numpy as np
+import pandas as pd
+import dicom
+import os
+import scipy.ndimage
+import matplotlib.pyplot as plt
+
+from skimage import measure, morphology
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+DATA_DIR = '../data/sample_images/'
+patients = os.listdir(DATA_DIR)
+patients.sort()
+
+
+def load_scan(path):
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key=lambda x: float(x.ImagePositionPatient[2]))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+
+    for s in slices:
+        s.SliceThickness = slice_thickness
+
+    return slices
+
+def get_pixels_hu(slices):
+    image = np.stack([s.pixel_array for s in slices])
+    # Convert to int16 (from sometimes int16),
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Set outside-of-scan pixels to 0
+    # The intercept is usually -1024, so air is approximately 0
+    image[image == -2000] = 0
+
+    # Convert to Hounsfield units (HU)
+    for slice_number in range(len(slices)):
+
+        intercept = slices[slice_number].RescaleIntercept
+        slope = slices[slice_number].RescaleSlope
+
+        if slope != 1:
+            image[slice_number] = slope * image[slice_number].astype(np.float64)
+            image[slice_number] = image[slice_number].astype(np.int16)
+
+        image[slice_number] += np.int16(intercept)
+
+    return np.array(image, dtype=np.int16)
+
+first_patient = load_scan(DATA_DIR + patients[0])
+first_patient_pixels = get_pixels_hu(first_patient)
+plt.figure(1)
+plt.hist(first_patient_pixels.flatten(), bins=80, color='c')
+plt.xlabel("Hounsfield Units (HU)")
+plt.ylabel("Frequency")
+plt.pause(0.1)
+
+# Show some slice in the middle
+plt.figure(2)
+plt.imshow(first_patient_pixels[80], cmap=plt.cm.gray)
+plt.pause(0.1)
+plt.show()
+
